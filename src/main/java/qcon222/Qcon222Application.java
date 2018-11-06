e178b07a7099e0b8ffaffacee5f9476026f344f0
Matt Cadorette <mcadorette@gmail.com>
2018-11-05 17:10:52 -0800

    Initial commit from Atomist

---
+++ src/main/java/qcon222/Qcon222Application.java
@@ -0,0 +1,12 @@
+package qcon222;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qcon222Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qcon222Application.class, args);
+	}
+}
---
+++ src/test/java/qcon222/Qcon222ApplicationTests.java
@@ -0,0 +1,16 @@
+package qcon222;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Qcon222ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
